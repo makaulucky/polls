f3a77a5c3cbf640b2b61a1b2a43a03fdfb19055d
makaulucky <makaulucky5@gmail.com>
2023-07-10 23:37:40 +0300

    Initial commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Question, Choice
+from django.urls import reverse
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+admin.site.site_header = "Admin | Insquare Technologies ~ Polls"
+admin.site.site_title = "Insquare Admin"
+admin.site.index_title = "Welcome to Insquare Technologies ~ Polls"
+
