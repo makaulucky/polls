754b5a45d366bc75bbba6fc07d588072890f9e30
makaulucky <makaulucky5@gmail.com>
2023-07-17 21:43:20 +0300

    Admin pages

--- polls/admin.py
+++ polls/admin.py
@@ -1,16 +1,34 @@
-from django.contrib import admin
-from .models import Question, Choice
-from django.urls import reverse
+# polls/admin.py
 
+from django.contrib import admin
+from django.utils import timezone
+import datetime
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self, obj):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= obj.pub_date <= now
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
 admin.site.site_header = "Admin | Insquare Technologies ~ Polls"
 admin.site.site_title = "Insquare Admin"
 admin.site.index_title = "Welcome to Insquare Technologies ~ Polls"
-
