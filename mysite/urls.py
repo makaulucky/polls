8f8e74f6c24c359d1710c3afdf8b87d549631f10
makaulucky <makaulucky5@gmail.com>
2023-07-20 13:35:20 +0300

    Cleaner code

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Home Page")
 
 urlpatterns = [
+    path('', index),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
