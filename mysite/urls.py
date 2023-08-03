a5d919888e4b1f6a446e5cf0cdaac2da14010377
makaulucky <makaulucky5@gmail.com>
2023-08-03 17:51:16 +0300

    Working

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,11 +6,7 @@ def index(request):
     return HttpResponse("Home Page")
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', include('polls.urls')),
-=======
     path('', index),
->>>>>>> 8f8e74f6c24c359d1710c3afdf8b87d549631f10
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
